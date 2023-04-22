072a4603a5f5e38c17927fb22e3e8a68d0bbd5e3
kevinlizzari <liz22004@byui.edu>
2023-04-22 17:55:30 +0200

    PREP1 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {name}, {name} {last_name}.");
     }
 }
