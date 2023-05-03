e8dd4971325773a281be625f2495138fec65998e
kevinlizzari <liz22004@byui.edu>
2023-05-03 23:21:24 +0200

    learning activity 2 completed

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+public class Resume
+{
+    private string _name;
+    private List<Job> _jobs = new List<Job>();
+
+    public Resume(string name)
+    {
+        _name = name;
+    }
+
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
